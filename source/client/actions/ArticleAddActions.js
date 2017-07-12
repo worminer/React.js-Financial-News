30370bdb352dc119b3458d952584f5d75cc6700b
radoslav-takev <takev.radoslav@gmail.com>
2017-07-12 20:15:03 +0300

    updated article add actions

--- source/client/actions/ArticleAddActions.js
+++ source/client/actions/ArticleAddActions.js
@@ -1,6 +1,6 @@
 import alt from '../alt'
 
-class MovieAddActions {
+class ArticleAddActions {
   constructor () {
     this.generateActions(
       'handleNameChange',
@@ -13,19 +13,19 @@ class MovieAddActions {
     )
   }
 
-  addMovie (data) {
+  addArticle (data) {
     let request = {
-      url: '/api/movies/add',
+      url: '/api/news/add',
       method: 'POST',
       data: JSON.stringify(data),
       contentType: 'application/json'
     }
     $.ajax(request)
-      .done(() => this.addMovieSuccess())
-      .fail((err) => this.addMovieFail(err))
+      .done(() => this.addArticleSuccess())
+      .fail((err) => this.addArticleFail(err))
 
     return true
   }
 }
 
-export default alt.createActions(MovieAddActions)
+export default alt.createActions(ArticleAddActions)
