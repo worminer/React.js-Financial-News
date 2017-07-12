3368a0286e1aa112f3d185a7efd748233cac52e2
radoslav-takev <takev.radoslav@gmail.com>
2017-07-12 06:25:12 +0300

    updated move controller

--- source/server/controllers/comment.js
+++ source/server/controllers/comment.js
@@ -5,10 +5,10 @@ module.exports = {
     get: (req, res) => {
       let movieId = req.params.movieId
 
-      Comment.find({movie: movieId}).
-        sort({dateCreated: -1}).
-        populate('author', '_id username').
-        then(comments => {
+      Comment.find({movie: movieId})
+        .sort({dateCreated: -1})
+        .populate('author', '_id username')
+        .then(comments => {
           res.status(200).send(comments)
         })
     },
@@ -16,13 +16,12 @@ module.exports = {
       let movieId = req.params.movieId
       let commentData = {
         movie: movieId,
-        content: req.body.content,
+        content: req.body.content
       }
 
       Comment.create(commentData).then(comment => {
         res.status(200).send({comment})
       })
-
-    },
-  },
+    }
+  }
 }
