9a9037d86eb14695c4dfbee1ed84bdb2d1bd7ba2
radoslav-takev <takev.radoslav@gmail.com>
2017-07-12 21:01:09 +0300

    updated rated articles by user

--- source/client/components/user/UserRatedArticles.js
+++ source/client/components/user/UserRatedArticles.js
@@ -1,19 +1,19 @@
 import React from 'react'
 
-import UserRatedMoviesPanel from './UserRatedMoviesPanel'
+import UserRatedArticlesPanel from './UserRatedArticlesPanel'
 
-export default class UserVotedMovies extends React.Component {
+export default class UserVotedArticles extends React.Component {
   constructor (props) {
     super(props)
 
     this.state = {
-      showRatedMoviesPanel: false
+      showRatedArticlesPanel: false
     }
   }
 
-  toggleRatedMovies () {
+  toggleRatedArticles () {
     this.setState(prevState => ({
-      showRatedMoviesPanel: !prevState.showRatedMoviesPanel
+      showRatedArticlesPanel: !prevState.showRatedArticlesPanel
     }))
   }
 
@@ -31,12 +31,12 @@ export default class UserVotedMovies extends React.Component {
         </div>
         <div className='pull-right btn-group'>
           <a className='btn btn-primary'
-            onClick={this.toggleRatedMovies.bind(this)}>
-            { this.state.showRatedMoviesPanel ? 'Hide' : 'Rated Movies' }
+            onClick={this.toggleRatedArticles.bind(this)}>
+            { this.state.showRatedArticlesPanel ? 'Hide' : 'Rated News' }
           </a>
         </div>
-        { this.state.showRatedMoviesPanel
-        ? <UserRatedMoviesPanel movies={this.props.votes} /> : null }
+        { this.state.showRatedArticlessPanel
+        ? <UserRatedArticlesPanel articles={this.props.votes} /> : null }
       </div>
     )
   }
